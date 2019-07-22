77b83969e2836c54c61e1755e645fecc9404e887
swcho9919 <swcho9919@gmail.com>
2019-07-22 11:35:27 +0900

    with url

--- collab/urls.py
+++ collab/urls.py
@@ -14,8 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from collab_git import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('new/', views.new, name='new'),
+    path('post/<int:post_pk>/', views.detail, name='detail'),
+    path('edit/<int:post_pk>/', views.edit, name='edit'),
+    path('delete/<int:post_pk>/', views.delete, name='delete'),
+    path('post/<int:post_pk>/<int:comment_pk>', views.delete_comment, name='delete_comment'),   
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/signup/', views.signup, name='signup'),
 ]
